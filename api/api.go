23cbf85704b1cd4da93eda4866ee3b9d3cf529e5
kimguny <kimguny12@gmail.com>
2024-05-24 17:57:19 +0900

    ✨ api url 설정

--- api/api.go
+++ api/api.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	login "guny-world-backend/api/login"
+	register "guny-world-backend/api/register"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setting(app *fiber.App) {
-
+	app.Post("/register", register.Register)
+	app.Post("/login", login.Login)
 }
